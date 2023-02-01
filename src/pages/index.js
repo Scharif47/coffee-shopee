11078168da348b0a0d6ac76965705ee44d9e3651
Scharif47 <scharif47@outlook.de>
2023-02-01 21:55:54 +0100

    hero changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Banner from "@/components/Banner";
+import heroImage from "../../public/static/hero-image.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,14 @@ export default function Home() {
       <main className={styles.main}>
         <Banner buttonText="View shops" handleOnClick={handleBannerOnClick} />
       </main>
+      <div className={styles.heroImage}>
+        <Image
+          src={heroImage}
+          alt="woman drinking coffee"
+          width={700}
+          height={400}
+        />
+      </div>
     </>
   );
 }
